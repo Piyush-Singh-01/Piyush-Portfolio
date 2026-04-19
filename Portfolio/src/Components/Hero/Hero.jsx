71eb522b96f36699e2657cff82d7ad900a78f0f7
PIYUSH SINGH <piyushsinghrajput9939@gmail.com>
2026-04-19 12:54:34 +0530

    Update Hero.jsx

--- Portfolio/src/Components/Hero/Hero.jsx
+++ Portfolio/src/Components/Hero/Hero.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import './Hero.css'
 import AnchorLink from "react-anchor-link-smooth-scroll";
-import resume from '../../../public/Resume.pdf'
+// import resume from '../../../public/Resume.pdf'
 import profile_image from '../../assets/profile.png';
 export default function Hero() {
    const handleResume = ()=>{
-      window.open("/resume", "_blank");
+      window.open("/RESUME.pdf", "_blank");
    }
   return (
     <div id='home' className="hero">
